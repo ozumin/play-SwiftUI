6f06531182f359a6542076476edd7d5f973f4596
ozumin <mizuo.n@gmail.com>
2020-07-05 18:33:56 +0900

    Until section 3

--- HelloWorld/ContentView.swift
+++ HelloWorld/ContentView.swift
@@ -8,9 +8,62 @@
 
 import SwiftUI
 
+struct Figures: View {
+    var body: some View {
+        ZStack {
+            Capsule().foregroundColor(Color.blue)
+            .frame(width: 200, height: 400)
+            .rotationEffect(.degrees(50))
+            .clipped()
+            Capsule()
+                .stroke(lineWidth: 10)
+                .foregroundColor(Color.red)
+            .frame(width: 200, height: 400)
+            .rotationEffect(.degrees(-45))
+                .position(x: 300, y: 400)
+        }
+    }
+}
+
 struct ContentView: View {
     var body: some View {
-        Text("Hello, World!")
+        VStack {
+            Figures()
+            HStack {
+                Text("Hello World")
+                    .font(.title)
+                    .foregroundColor(Color.red)
+                    .rotation3DEffect(.degrees(45), axis: (x: 0, y: 1, z: 0))
+                Text(/*@START_MENU_TOKEN@*/"Placeholder"/*@END_MENU_TOKEN@*/)
+                    .offset(x: 20, y:0)
+            }
+            .padding(.top, 80)
+            Spacer()
+            HStack {
+                Text("Hello\nWorld")
+                    .font(.system(size:100))
+                Text("Place\nholder")
+                    .fontWeight(.ultraLight)
+                    .offset(x: 20, y:0)
+            }
+            Spacer()
+            Text("あああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああ")
+                .multilineTextAlignment(.center)
+                .lineLimit(2)
+                .frame(width: 200.0)
+                .border(Color.green, width: 5)
+            Image("yoshiyoshi")
+            .resizable()
+                .aspectRatio(contentMode: .fill)
+                .scaleEffect(1.5)
+                .offset(x: -70, y: 30)
+                .frame(width: 300, height: 200)
+            .clipped()
+            .overlay(
+                Text("Text on image")
+                    .font(.title)
+            )
+        }
     }
 }
 
